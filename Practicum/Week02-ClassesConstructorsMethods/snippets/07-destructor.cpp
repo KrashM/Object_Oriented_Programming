3f5ac816238afc4ea850f3201d8e5c506384e410
Vasil Zahariev <vasilzahariev1707@abv.bg>
2023-03-09 15:28:48 +0200

    Update 07-destructor.cpp

--- Practicum/Week02-ClassesConstructorsMethods/snippets/07-destructor.cpp
+++ Practicum/Week02-ClassesConstructorsMethods/snippets/07-destructor.cpp
@@ -7,32 +7,55 @@ class Teacher
 private:
     char* name;
     char* subject;
-
+    
 public:
     Teacher(const char* name, const char* subject)
+        : name(nullptr), subject(nullptr)
     {
         std::cout << "Inside constuctor" << std::endl;
 
+        setName(name);
+
+        if (!this->name) {            
+            return;
+        }
+
+        setSubject(subject);
+
+        if (!this->subject) {
+            delete[] this->name;
+
+            return;
+        }
+    }
+
+    void setName(const char* name) {
+        delete[] this->name;
         this->name = new (std::nothrow) char[strlen(name) + 1];
+
         if (!this->name)
         {
             std::cout << "Memory problem" << std::endl;
             return;
         }
+        
         strcpy(this->name, name);
+    }
 
+    void setSubject(const char* subject) {
+        delete[] this->subject;
         this->subject = new (std::nothrow) char[strlen(subject) + 1];
+        
         if (!this->subject)
         {
-            std::cout << "Memory problem" << std::endl;
-
-            delete[] this->name; 
+            std::cout << "Memory problem" << std::endl; 
             return;
         }
+        
         strcpy(this->subject, subject);
     }
 
-    void print()
+    void print() const
     {
         std::cout << "My name is " << this->name << " and I am teaching " << this->subject << "." << std::endl;
     }
@@ -48,9 +71,9 @@ public:
 
 int main ()
 {
-    Teacher teacher("Borislav Draganov", "DIS2");
+    // Teacher teacher("Borislav Draganov", "DIS2");
 
-    teacher.print();
+    // teacher.print();
 
     Teacher* dynamicallyAllocatedTeacher = new (std::nothrow) Teacher("Bogdan Alexandrov", "Geometry");
     if (!dynamicallyAllocatedTeacher)
@@ -60,7 +83,7 @@ int main ()
     }
 
     dynamicallyAllocatedTeacher->print();
-    delete dynamicallyAllocatedTeacher;
 
+    delete dynamicallyAllocatedTeacher;
     return 0;
 }
